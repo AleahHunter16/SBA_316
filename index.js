c76b846f1fa154476d0fcc541be5486f40ec203e
AleahHunter16 <AleahHunter16@gmail.com>
2024-10-21 19:57:13 -0400

    commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.addEventListener
+document.addEventListener('DOMContentLoaded',() =>{
+    const form = document.getElementById('bookingForm');
+    const confirmationDiv = document.getElementById('confirmation');
+    const confirmationMessage = document.getElementById('confirmationMessage');
+    const newBookingButton = document.getElementById('newBooking');
+    //*Submission form//
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+    })
+})
